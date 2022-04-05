39657d15335c8664dd082cd92fad5d5225349cbb
EJBEAN2op <danonymousarmy@gmail.com>
2022-04-05 17:45:16 +0530

    rem dist

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.http = void 0;
-const http_1 = __importDefault(require("./core/http"));
-exports.http = http_1.default;
-exports.default = { http: http_1.default };
