2971c956f6658036596401748ac8e474d554c9c9
EJBEAN2op <danonymousarmy@gmail.com>
2022-04-05 19:03:49 +0530

    rem dist

--- core/http/index.ts
+++
@@ -1,7 +0,0 @@
-import Endpoint from "./src/Endpoint";
-import Response from "./src/Response";
-import Server from "./src/Server";
-import statusCodes from "./src/statusCodes";
-
-export { Server, Response, statusCodes, Endpoint };
-export default { Server, Response, statusCodes, Endpoint };
--- modules/Logger.ts
+++
@@ -1,7 +0,0 @@
-class Logger {
-    static log(...message: string[]) {
-        console.log('[adminhq]', ...message);
-    }
-}
-
-export default Logger;
